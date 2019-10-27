6bb8c56a5b8d12e947100f618ccd8d1910308479
shawnantonucci <shawnantonucci2018@gmail.com>
2019-10-26 20:53:42 -0700

    refactored playermovement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 10.0f;
-    private Rigidbody playerRb;
     private float zBound = 6;
+    private Rigidbody playerRb;
 
     // Start is called before the first frame update
     void Start()
@@ -17,12 +17,21 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        MovePlayer();
+        ConstrainPlayerPosition();
+    }
+
+    void MovePlayer()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
         playerRb.AddForce(Vector3.forward * speed * verticalInput);
         playerRb.AddForce(Vector3.right * speed * horizontalInput);
+    }
 
+    void ConstrainPlayerPosition()
+    {
         if (transform.position.z < -zBound)
         {
             transform.position = new Vector3(transform.position.x, transform.position.y, -zBound);
